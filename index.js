6e50d5fbbefc72e0c4c40da90672b4ebacbe3052
Matt Peperell <mattp@automationlogic.com>
2016-09-16 11:57:47 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+        res.send('Hello World!');
+});
+
+var port = process.env.PORT;
+app.listen(port, function () {
+        console.log('App listening on port ' + port);
+});
